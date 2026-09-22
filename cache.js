bae80f7fe754e947a6f62a54455868c66feaabe6
Nithesh <31339538+nitheshurao@users.noreply.github.com>
2026-09-22 20:46:12 +0530

    cod

--- cache.js
+++ cache.js
@@ -1,18 +1,8 @@
-function LRU(){
-    let cache={}
-
-    function getkey(key){
-        // if(!cache[key]) return -1
-        return cache[key]
+class cache{
+    constructor(size){
+        this.size=size
+        this.cache={}
+        this.order=[]
     }
-     function addkey(key,v){
-        if(cache[key]){
-            cache[key]=v
-        }
-     }
-     return {getkey,addkey}
+
 }
-let neL= LRU()
-neL.addkey("1","hiii")
-neL.getkey("1")
-console.log("------",neL.getkey("1"))
